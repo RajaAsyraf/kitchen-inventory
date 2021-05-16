1434a28d830e8a9c026e89f2590b2fa003ec6b6b
Raja Mohamad Asyraf <rmasyraf@gmail.com>
2021-05-16 21:54:15 +0800

    Refactor auth to use helper

--- app/Http/Controllers/KitchenItemController.php
+++ app/Http/Controllers/KitchenItemController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class KitchenItemController extends Controller
 {
     public function create($id)
     {
-        $kitchen = Auth::user()->kitchens()->findOrFail($id);
+        $kitchen = auth()->user()->kitchens()->findOrFail($id);
 
         return view('items.create', ['kitchen' => $kitchen]);
     }
