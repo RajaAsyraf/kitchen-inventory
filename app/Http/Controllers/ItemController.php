acb6bd5bcc80e4884e2a881416de08bb2483ebb8
Raja Mohamad Asyraf <rmasyraf@gmail.com>
2021-05-19 22:55:50 +0800

    Add softdelete to item

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $item = Item::with('kitchen')->findOrFail($id);
+
+        abort_unless($item->isVisibleTo(auth()->user()), 404);
+
+        dd($item);
+
+        return view('items.show', compact('item'));
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy($id)
+    {
+        $item = Item::findOrFail($id);
+
+        abort_unless($item->isVisibleTo(auth()->user()), 404);
+
+        $item->delete();
+
+        return redirect()->route('kitchen.index');
+    }
 }
