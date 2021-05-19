5a13ed79bbe79a24702b7de3fde3a647fd806f6a
Raja Mohamad Asyraf <rmasyraf@gmail.com>
2021-05-19 23:41:31 +0800

    Display old request value & error message

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -24,36 +24,48 @@
                                         <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                             <div class="flex flex-col">
                                                 <label class="leading-loose">Name</label>
-                                                <input type="text" name="name" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Item name" required>
+                                                <input type="text" name="name" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Item name" value="{{ old('name') }}" required>
+                                                @error('name')
+                                                    <small class="text-red-500">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                             <div class="flex items-center space-x-4">
                                                 <div class="flex flex-col">
                                                     <label class="leading-loose">Quantity</label>
                                                     <div class="relative focus-within:text-gray-600 text-gray-400">
-                                                        <input type="number" name="quantity" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" value="1" required>
+                                                        <input type="number" name="quantity" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" value="{{ old('quantity') ?? 1 }}">
+                                                        @error('quantity')
+                                                            <small class="text-red-500">{{ $message }}</small>
+                                                        @enderror
                                                     </div>
                                                 </div>
                                                 <div class="flex flex-col w-full">
                                                     <label class="leading-loose">Unit</label>
                                                     <div class="relative focus-within:text-gray-600 text-gray-400">
                                                         <select name="unit" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" required>
-                                                            <option value="piece">Piece</option>
-                                                            <option value="pack">Pack</option>
-                                                            <option value="bottle">Bottle</option>
-                                                            <option value="can">Can</option>
-                                                            <option value="percentage">Percentage (%)</option>
+                                                            <option value="piece" {{ old('unit') == 'piece' ? 'selected' : '' }}>Piece</option>
+                                                            <option value="pack" {{ old('unit') == 'pack' ? 'selected' : '' }}>Pack</option>
+                                                            <option value="bottle" {{ old('unit') == 'bottle' ? 'selected' : '' }}>Bottle</option>
+                                                            <option value="can" {{ old('unit') == 'can' ? 'selected' : '' }}>Can</option>
+                                                            <option value="percentage" {{ old('unit') == 'percentage' ? 'selected' : '' }}>Percentage (%)</option>
                                                         </select>
+                                                        @error('unit')
+                                                            <small class="text-red-500">{{ $message }}</small>
+                                                        @enderror
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="flex flex-col">
                                                 <label class="leading-loose">Expired At</label>
                                                 <div class="relative focus-within:text-gray-600 text-gray-400">
-                                                    <input type="date" name="expired_at" class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" required>
+                                                    <input type="date" name="expired_at" class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" value="{{ old('expired_at') }}" required>
                                                     <div class="absolute left-3 top-2">
                                                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                                                     </div>
                                                 </div>
+                                                @error('expired_at')
+                                                    <small class="text-red-500">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                             <div class="pt-4 flex items-center space-x-4">
                                                 <a href="{{ route('kitchen.index') }}" class="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none">
--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -19,36 +19,49 @@
                                         <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                             <div class="flex flex-col">
                                                 <label class="leading-loose">Name</label>
-                                                <input type="text" name="name" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Item name" value="{{ $item->name }}" required>
+                                                <input type="text" name="name" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Item name" value="{{ old('name') ?? $item->name }}" required>
+                                                @error('name')
+                                                    <small class="text-red-500">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                             <div class="flex items-center space-x-4">
                                                 <div class="flex flex-col">
                                                     <label class="leading-loose">Quantity</label>
                                                     <div class="relative focus-within:text-gray-600 text-gray-400">
-                                                        <input type="number" name="quantity" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" value="{{ $item->quantity }}" required>
+                                                        <input type="number" name="quantity" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" value="{{ old('quantity') ?? $item->quantity }}" required>
+                                                        @error('name')
+                                                            <small class="text-red-500">{{ $message }}</small>
+                                                        @enderror
                                                     </div>
                                                 </div>
                                                 <div class="flex flex-col w-full">
                                                     <label class="leading-loose">Unit</label>
                                                     <div class="relative focus-within:text-gray-600 text-gray-400">
+                                                        @php($selectedItem = old('unit') ?? $item->unit)
                                                         <select name="unit" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" required>
-                                                            <option value="piece" {{ $item->unit == 'piece' ? 'selected' : '' }}>Piece</option>
-                                                            <option value="pack" {{ $item->unit == 'pack' ? 'selected' : '' }}>Pack</option>
-                                                            <option value="bottle" {{ $item->unit == 'bottle' ? 'selected' : '' }}>Bottle</option>
-                                                            <option value="can" {{ $item->unit == 'can' ? 'selected' : '' }}>Can</option>
-                                                            <option value="percentage" {{ $item->unit == 'percentage' ? 'selected' : '' }}>Percentage (%)</option>
+                                                            <option value="piece" {{ $selectedItem == 'piece' ? 'selected' : '' }}>Piece</option>
+                                                            <option value="pack" {{ $selectedItem == 'pack' ? 'selected' : '' }}>Pack</option>
+                                                            <option value="bottle" {{ $selectedItem == 'bottle' ? 'selected' : '' }}>Bottle</option>
+                                                            <option value="can" {{ $selectedItem == 'can' ? 'selected' : '' }}>Can</option>
+                                                            <option value="percentage" {{ $selectedItem == 'percentage' ? 'selected' : '' }}>Percentage (%)</option>
                                                         </select>
+                                                        @error('unit')
+                                                            <small class="text-red-500">{{ $message }}</small>
+                                                        @enderror
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="flex flex-col">
                                                 <label class="leading-loose">Expired At</label>
                                                 <div class="relative focus-within:text-gray-600 text-gray-400">
-                                                    <input type="date" name="expired_at" class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" value="{{ $item->expired_at->format('Y-m-d') }}" required>
+                                                    <input type="date" name="expired_at" class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" value="{{ old('expired_at') ?? $item->expired_at->format('Y-m-d') }}" required>
                                                     <div class="absolute left-3 top-2">
                                                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                                                     </div>
                                                 </div>
+                                                @error('expired_at')
+                                                    <small class="text-red-500">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                             <div class="pt-4 flex items-center space-x-4">
                                                 <a href="{{ route('kitchen.index') }}" class="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none">
